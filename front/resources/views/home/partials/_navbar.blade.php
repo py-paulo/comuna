6f8e357de526e174efcea06e4557aa7cb02e85b4
Yane <yanelisset4@gmail.com>
2021-01-23 15:37:41 -0300

    feat(home): add section navbar blade and remove blade main

--- front/resources/views/home/partials/_navbar.blade.php
+++ front/resources/views/home/partials/_navbar.blade.php
@@ -5,115 +5,22 @@
             <div class="c-navbar__zone c-navbar__zone--left c-navbar__zone--push-right">
                 <nav class="menu-main-menu-container">
                     <ul id="menu-1" class="menu">
-                        <li id="menu-item-443" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-443"><a href="https://demos.pixelgrade.com/noto/" aria-current="page">Articles</a></li>
+                        <li id="menu-item-443" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-443"><a href="#" aria-current="page">Articles</a></li>
                         <li id="menu-item-41" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-41"><a href="#">Style Guide</a>
                             <ul class="sub-menu">
-                                <li id="menu-item-47" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-47"><a href="https://demos.pixelgrade.com/noto/style-guide/testing-type/">Typography</a></li>
-                                <li id="menu-item-103" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-103"><a href="https://demos.pixelgrade.com/noto/style-guide/testing-images-alignment/">Images</a></li>
-                                <li id="menu-item-133" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-133"><a href="https://demos.pixelgrade.com/noto/style-guide/testing-forms/">Forms</a></li>
+                                <li id="menu-item-47" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-47"><a href="#">Typography</a></li>
+                                <li id="menu-item-103" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-103"><a href="#">Images</a></li>
+                                <li id="menu-item-133" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-133"><a href="#">Forms</a></li>
                             </ul>
                         </li>
                         <li id="menu-item-463" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-463"><a href="#">Posts</a>
                             <ul class="sub-menu">
-                                <li id="menu-item-507" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-507"><a href="https://demos.pixelgrade.com/noto/2018/06/23/paul-kafner-breaking-the-mold/">Standard</a></li>
-                                <li id="menu-item-470" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-470"><a href="https://demos.pixelgrade.com/noto/2018/06/21/idea-selfies-with-purpose/">Portrait</a></li>
-                                <li id="menu-item-472" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-472"><a href="https://demos.pixelgrade.com/noto/2018/06/27/sabinus-and-his-loyal-dog/">Comments</a></li>
+                                <li id="menu-item-507" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-507"><a href="#">Standard</a></li>
+                                <li id="menu-item-470" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-470"><a href="#">Portrait</a></li>
+                                <li id="menu-item-472" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-472"><a href="#">Comments</a></li>
                             </ul>
                         </li>
-                        <li id="menu-item-447" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-447"><a href="https://demos.pixelgrade.com/noto/category/lifestyle/">Lifestyle</a></li>
-                    </ul>
-                </nav>
-            </div><!-- .c-navbar__zone -->
-            <div class="search-trigger">
-                <button class="js-search-trigger">
-                    <svg width="26" height="27" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M18.1737 16.9359L26 24.7618l-1.6944 1.6944-7.826-7.8263c-1.733 1.3672-3.922 2.1836-6.3002 2.1836C4.5574 20.8135 0 16.2558 0 10.6356 0 5.0136 4.5574.4562 10.1794.4562c5.6201 0 10.1779 4.5574 10.1779 10.1794 0 2.3782-.8164 4.5673-2.1836 6.3003zm-7.9943 1.4832c4.2977 0 7.7835-3.4858 7.7835-7.7835 0-4.2992-3.4858-7.784-7.7835-7.784-4.2992 0-7.784 3.4848-7.784 7.784 0 4.2977 3.4848 7.7835 7.784 7.7835z" fill="currentColor" fill-rule="evenodd"></path>
-                    </svg>
-                    <span class="screen-reader-text">Search</span>
-                </button>
-            </div>
-            <div class="c-navbar__content">
-                <div class="c-navbar__zone c-navbar__zone--left c-navbar__zone--push-right">
-                    <nav class="menu-main-menu-container">
-                        <ul id="menu-1" class="menu">
-                            <li id="menu-item-443" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-443"><a href="https://demos.pixelgrade.com/noto/" aria-current="page">Articles</a></li>
-                            <li id="menu-item-41" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-41"><a href="#">Style Guide</a>
-                                <ul class="sub-menu">
-                                    <li id="menu-item-47" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-47"><a href="https://demos.pixelgrade.com/noto/style-guide/testing-type/">Typography</a></li>
-                                    <li id="menu-item-103" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-103"><a href="https://demos.pixelgrade.com/noto/style-guide/testing-images-alignment/">Images</a></li>
-                                    <li id="menu-item-133" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-133"><a href="https://demos.pixelgrade.com/noto/style-guide/testing-forms/">Forms</a></li>
-                                </ul>
-                            </li>
-                            <li id="menu-item-463" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-463"><a href="#">Posts</a>
-                                <ul class="sub-menu">
-                                    <li id="menu-item-507" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-507"><a href="https://demos.pixelgrade.com/noto/2018/06/23/paul-kafner-breaking-the-mold/">Standard</a></li>
-                                    <li id="menu-item-470" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-470"><a href="https://demos.pixelgrade.com/noto/2018/06/21/idea-selfies-with-purpose/">Portrait</a></li>
-                                    <li id="menu-item-472" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-472"><a href="https://demos.pixelgrade.com/noto/2018/06/27/sabinus-and-his-loyal-dog/">Comments</a></li>
-                                </ul>
-                            </li>
-                            <li id="menu-item-447" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-447"><a href="https://demos.pixelgrade.com/noto/category/lifestyle/">Lifestyle</a></li>
-                        </ul>
-                    </nav>
-                </div>
-                <div class="c-navbar__zone c-navbar__zone--right" style="opacity: 1;">
-                    <nav class="menu-social-menu-container">
-                        <ul id="menu-2" class="menu">
-                            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-613"><a href="https://www.facebook.com/pixelgrademedia/">Facebook</a></li>
-                            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-444"><a href="https://twitter.com/pixelgrade">Twitter</a></li>
-                            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-615"><a href="https://pinterest.com/pixelgrade/">Pinterest</a></li>
-                            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-445"><a href="https://demos.pixelgrade.com/noto/feed">Feed</a></li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-            <div class="c-navbar__shadow"></div>
-        </div>
-        <div class="c-noto c-noto--body" style="margin-bottom: 246px; margin-top: 84px;">
-            <input class="c-navbar__checkbox" id="menu-toggle" type="checkbox">
-            <label class="c-navbar__label u-header-sides-spacing" for="menu-toggle">
-                <span class="c-navbar__label-icon"><span class="c-burger c-burger--fade">
-                        <b class="c-burger__slice c-burger__slice--top"></b>
-                        <b class="c-burger__slice c-burger__slice--middle"></b>
-                        <b class="c-burger__slice c-burger__slice--bottom"></b>
-                    </span>
-                </span>
-                <span class="c-navbar__label-text screen-reader-text">Menu</span>
-            </label><!-- .c-navbar__label -->
-            <div class="search-trigger">
-                <button class="js-search-trigger">
-                    <svg width="26" height="27" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M18.1737 16.9359L26 24.7618l-1.6944 1.6944-7.826-7.8263c-1.733 1.3672-3.922 2.1836-6.3002 2.1836C4.5574 20.8135 0 16.2558 0 10.6356 0 5.0136 4.5574.4562 10.1794.4562c5.6201 0 10.1779 4.5574 10.1779 10.1794 0 2.3782-.8164 4.5673-2.1836 6.3003zm-7.9943 1.4832c4.2977 0 7.7835-3.4858 7.7835-7.7835 0-4.2992-3.4858-7.784-7.7835-7.784-4.2992 0-7.784 3.4848-7.784 7.784 0 4.2977 3.4848 7.7835 7.784 7.7835z" fill="currentColor" fill-rule="evenodd"></path>
-                    </svg>
-                    <span class="screen-reader-text">Search</span>
-                </button>
-            </div>
-            <div class="c-navbar__zone c-navbar__zone--middle c-navbar__zone--branding" style="opacity: 1;">
-                <div class="header nav">
-                    <div class="c-branding">
-                        <div class="c-profile-photo">
-                            <div class="c-profile-photo__default">
-                                <a href="https://demos.pixelgrade.com/noto/" class="profile-photo-link" rel="home" itemprop="url"><img width="536" height="536" src="https://cdn.demos.pixelgrade.com/wp-content/uploads/sites/67/2018/07/noto-logo-avatar.jpg" class="profile-photo" alt="Noto" loading="lazy" itemprop="logo" srcset="https://cdn.demos.pixelgrade.com/wp-content/uploads/sites/67/2018/07/noto-logo-avatar.jpg 536w, https://cdn.demos.pixelgrade.com/wp-content/uploads/sites/67/2018/07/noto-logo-avatar-150x150.jpg 150w, https://cdn.demos.pixelgrade.com/wp-content/uploads/sites/67/2018/07/noto-logo-avatar-300x300.jpg 300w, https://cdn.demos.pixelgrade.com/wp-content/uploads/sites/67/2018/07/noto-logo-avatar-450x450.jpg 450w" sizes="(max-width: 536px) 100vw, 536px" data-attachment-id="517" data-permalink="https://demos.pixelgrade.com/noto/noto-logo-avatar/" data-orig-file="https://cdn.demos.pixelgrade.com/wp-content/uploads/sites/67/2018/07/noto-logo-avatar.jpg" data-orig-size="536,536" data-comments-opened="1" data-image-meta="{&quot;aperture&quot;:&quot;0&quot;,&quot;credit&quot;:&quot;&quot;,&quot;camera&quot;:&quot;&quot;,&quot;caption&quot;:&quot;&quot;,&quot;created_timestamp&quot;:&quot;0&quot;,&quot;copyright&quot;:&quot;&quot;,&quot;focal_length&quot;:&quot;0&quot;,&quot;iso&quot;:&quot;0&quot;,&quot;shutter_speed&quot;:&quot;0&quot;,&quot;title&quot;:&quot;&quot;,&quot;orientation&quot;:&quot;0&quot;}" data-image-title="noto-logo-avatar" data-image-description="" data-medium-file="https://cdn.demos.pixelgrade.com/wp-content/uploads/sites/67/2018/07/noto-logo-avatar-300x300.jpg" data-large-file="https://cdn.demos.pixelgrade.com/wp-content/uploads/sites/67/2018/07/noto-logo-avatar.jpg"></a>
-                            </div>
-                        </div>
-                        <div class="c-logo">
-                            <div class="c-logo__default">
-                                <a href="https://demos.pixelgrade.com/noto/" class="custom-logo-link" rel="home" aria-current="page"><img width="311" height="50" src="https://cdn.demos.pixelgrade.com/wp-content/uploads/sites/67/2018/07/logo-notomya.png" class="custom-logo" alt="Noto" srcset="https://cdn.demos.pixelgrade.com/wp-content/uploads/sites/67/2018/07/logo-notomya.png 311w, https://cdn.demos.pixelgrade.com/wp-content/uploads/sites/67/2018/07/logo-notomya-300x48.png 300w" sizes="(max-width: 311px) 100vw, 311px" data-attachment-id="599" data-permalink="https://demos.pixelgrade.com/noto/logo-notomya/" data-orig-file="https://cdn.demos.pixelgrade.com/wp-content/uploads/sites/67/2018/07/logo-notomya.png" data-orig-size="311,50" data-comments-opened="1" data-image-meta="{&quot;aperture&quot;:&quot;0&quot;,&quot;credit&quot;:&quot;&quot;,&quot;camera&quot;:&quot;&quot;,&quot;caption&quot;:&quot;&quot;,&quot;created_timestamp&quot;:&quot;0&quot;,&quot;copyright&quot;:&quot;&quot;,&quot;focal_length&quot;:&quot;0&quot;,&quot;iso&quot;:&quot;0&quot;,&quot;shutter_speed&quot;:&quot;0&quot;,&quot;title&quot;:&quot;&quot;,&quot;orientation&quot;:&quot;0&quot;}" data-image-title="logo-notomya" data-image-description="" data-medium-file="https://cdn.demos.pixelgrade.com/wp-content/uploads/sites/67/2018/07/logo-notomya-300x48.png" data-large-file="https://cdn.demos.pixelgrade.com/wp-content/uploads/sites/67/2018/07/logo-notomya.png"></a>
-                            </div>
-                        </div>
-                        <a class="c-logo__title" href="https://demos.pixelgrade.com/noto/" rel="home">
-                            <h1 class="site-title" style="font-size: 121.043px;">Noto</h1>
-                        </a>
-                        <p class="site-description site-description-text">Spreading the face of Anne throughout the known universe and beyond.</p>
-                    </div><!-- .c-branding -->
-                </div>
-            </div><!-- .c-navbar__zone -->
-            <div class="c-navbar__zone c-navbar__zone--right" style="opacity: 1;">
-                <nav class="menu-social-menu-container">
-                    <ul id="menu-2" class="menu">
-                        <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-613"><a href="https://www.facebook.com/pixelgrademedia/">Facebook</a></li>
-                        <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-444"><a href="https://twitter.com/pixelgrade">Twitter</a></li>
-                        <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-615"><a href="https://pinterest.com/pixelgrade/">Pinterest</a></li>
-                        <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-445"><a href="https://demos.pixelgrade.com/noto/feed">Feed</a></li>
+                        <li id="menu-item-447" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-447"><a href="#">Lifestyle</a></li>
                     </ul>
                 </nav>
             </div><!-- .c-navbar__zone -->
